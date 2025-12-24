a3f312e01f0fd30a103600e162d2070ed6c85e8b
Vladimir Shchur <odindafna2006@rambler.ru>
2025-12-24 14:47:28 -0800

    Step04 refactoring

--- examples/Agent_Step04_HumanApproval/Program.fs
+++ examples/Agent_Step04_HumanApproval/Program.fs
@@ -84,25 +84,28 @@ module Target =
         let threadId = agent.GetNewThread()
         let sendMessage = agent.GetSendThreadMessage(threadId)
         let sendContents = agent.GetSendThreadContents(threadId)
-        let mutable userInputRequests = [||]
+        let rec handleResponse (response: AgentRunResponse) =
+            task {
+                let userInputRequests = response.UserInputRequests |> Seq.toArray
+                if userInputRequests.Length > 0 then
+                    let! response =
+                        userInputRequests
+                        |> Array.choose (function
+                            | :? FunctionApprovalRequestContent as functionApprovalRequest ->
+                                Console.WriteLine(
+                                    "The agent would like to invoke the following function, please reply Y to approve: Name {0}",
+                                    functionApprovalRequest.FunctionCall.Name)
+                                let approved = Console.ReadLine() = "Y"
+                                functionApprovalRequest.CreateResponse(approved) :> AIContent |> Some
+                            | _ -> None)
+                        |> sendContents
+                    return! handleResponse response
+                else
+                    printfn $"\nAgent: {response}"
+            }
         task {
             let! response = sendMessage "What is the weather like in Amsterdam?"
-            userInputRequests <- response.UserInputRequests |> Seq.toArray
-            while userInputRequests.Length > 0 do
-                // Ask the user to approve each function call request.
-                let userInputResponses =
-                    userInputRequests
-                    |> Array.choose (function
-                        | :? FunctionApprovalRequestContent as functionApprovalRequest ->
-                            printfn
-                                $"The agent would like to invoke the following function, please reply Y to approve: Name %s{functionApprovalRequest.FunctionCall.Name}"
-                            let approved = Console.ReadLine() = "Y"
-                            functionApprovalRequest.CreateResponse(approved) :> AIContent |> Some
-                        | _ -> None
-                    )
-                let! response = sendContents userInputResponses
-                userInputRequests <- response.UserInputRequests |> Seq.toArray
-                printfn "\nAgent: %O" response
+            do! handleResponse response
         }
         |> _.Wait()
 
