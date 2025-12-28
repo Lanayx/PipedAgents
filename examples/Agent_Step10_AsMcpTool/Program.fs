dcdaeef676032172464a2d80aab12fd71c8071df
Vladimir Shchur <odindafna2006@rambler.ru>
2025-12-27 22:52:57 -0800

    Try adding step10 baseline

---
+++ examples/Agent_Step10_AsMcpTool/Program.fs
@@ -0,0 +1,50 @@
+#nowarn "57"
+
+open System
+open System.ClientModel
+open System.ClientModel.Primitives
+open FunAgents.MAF
+open FunAgents.MAF.OpenAI
+open Microsoft.Agents.AI
+open FSharp.Control
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open ModelContextProtocol.Server
+open OpenAI.Responses
+open Shared
+
+module Baseline =
+
+    let run() =
+        let key = ApiKeyCredential(
+            Environment.GetEnvironmentVariable("OPENAI_API_KEY")
+        )
+        let httpClient = getLoggingHttpClient()
+        let options = OpenAI.OpenAIClientOptions(
+            Transport = new HttpClientPipelineTransport(httpClient),
+            Endpoint = Uri(Environment.GetEnvironmentVariable "OPENAI_BASE_URL")
+        )
+        let client = OpenAI.OpenAIClient(key, options)
+        let responseClient = client.GetResponsesClient(Environment.GetEnvironmentVariable "MODEL_ID")
+        let agent =
+            responseClient
+                .CreateAIAgent(
+                    instructions = "You are good at telling jokes, and you always start each joke with 'Aye aye, captain!'.",
+                    name = "Joker")
+
+        task {
+            // Convert the agent to an AIFunction and then to an MCP tool.
+            // The agent name and description will be used as the mcp tool name and description.
+            let tool = McpServerTool.Create(agent.AsAIFunction());
+
+            // Register the MCP server with StdIO transport and expose the tool via the server.
+            let builder = Host.CreateEmptyApplicationBuilder(null);
+            builder.Services
+                .AddMcpServer()
+                .WithStdioServerTransport()
+                .WithTools([tool]) |> ignore
+
+            do! builder.Build().RunAsync()
+        } |> _.GetAwaiter().GetResult()
+
+Baseline.run()
