48b58f376ac6a21227b6a72064c0cbc0b6884e48
Vladimir Shchur <odindafna2006@rambler.ru>
2026-01-01 17:19:27 -0800

    Simplified CreateAgent typing

--- src/PipedAgents.MAF.OpenAI/Core.fs
+++ src/PipedAgents.MAF.OpenAI/Core.fs
@@ -4,15 +4,14 @@ namespace PipedAgents.MAF.OpenAI
 
 open System
 open System.ClientModel
+open System.Runtime.CompilerServices
 open PipedAgents.MAF
 open Microsoft.Agents.AI
 open Microsoft.Extensions.AI
 open OpenAI.Chat
 open OpenAI.Responses
 
-type IOpenAiChatClient<'T> = interface end
 type ResponsesChatClient(chatClient: IChatClient) =
-    interface IOpenAiChatClient<CreateResponseOptions>
     interface IChatClient with
         member this.GetResponseAsync(messages, options, ct) = chatClient.GetResponseAsync(messages, options, ct)
         member this.GetStreamingResponseAsync(messages, options, ct) = chatClient.GetStreamingResponseAsync(messages, options, ct)
@@ -20,35 +19,27 @@ type ResponsesChatClient(chatClient: IChatClient) =
         member this.Dispose() = chatClient.Dispose()
 
 type ChatCompletionsChatClient(chatClient: IChatClient) =
-    interface IOpenAiChatClient<ChatCompletionOptions>
     interface IChatClient with
         member this.GetResponseAsync(messages, options, ct) = chatClient.GetResponseAsync(messages, options, ct)
         member this.GetStreamingResponseAsync(messages, options, ct) = chatClient.GetStreamingResponseAsync(messages, options, ct)
         member this.GetService(serviceType, serviceKey) = chatClient.GetService(serviceType, serviceKey)
         member this.Dispose() = chatClient.Dispose()
 
-type OpenAiChatClient =
-    | ResponsesClient of ResponsesChatClient
-    | ChatCompletionsClient of ChatCompletionsChatClient
-    member this.CreateAgent(chatOptions: AgentOptions<'T>) =
-        match this with
-        | ResponsesClient agent ->
-            agent.CreateAIAgent(chatOptions.ToAgentOptions())
-        | ChatCompletionsClient agent ->
-            agent.CreateAIAgent(chatOptions.ToAgentOptions())
-    member this.AddMiddleware(runMiddleware, ?streamingRunMiddleware) =
-        match this with
-        | ResponsesClient client ->
-            client.AsBuilder().Use(runMiddleware, (streamingRunMiddleware |> Option.toObj))
-                .Build() |> fun chatClient -> new ResponsesChatClient(chatClient) |> OpenAiChatClient.ResponsesClient
-        | ChatCompletionsClient client ->
-            client.AsBuilder().Use(runMiddleware, (streamingRunMiddleware |> Option.toObj))
-                .Build() |> fun chatClient -> new ChatCompletionsChatClient(chatClient) |> OpenAiChatClient.ChatCompletionsClient
-    interface IDisposable with
-        member this.Dispose() =
-            match this with
-            | ResponsesClient client -> (client :> IDisposable).Dispose()
-            | ChatCompletionsClient client -> (client :> IDisposable).Dispose()
+type ChatClientExtensions =
+    [<Extension>]
+    static member CreateAgent(chatClient: ResponsesChatClient, chatOptions: AgentOptions<CreateResponseOptions>) =
+        chatClient.CreateAIAgent(chatOptions.ToAgentOptions())
+    [<Extension>]
+    static member CreateAgent(chatClient: ChatCompletionsChatClient, chatOptions: AgentOptions<ChatCompletionOptions>) =
+        chatClient.CreateAIAgent(chatOptions.ToAgentOptions())
+    [<Extension>]
+    static member AddMiddleware(chatClient: ResponsesChatClient, runMiddleware, ?streamingRunMiddleware) =
+        chatClient.AsBuilder().Use(runMiddleware, (streamingRunMiddleware |> Option.toObj)).Build()
+        |> fun chatClient -> new ResponsesChatClient(chatClient)
+    [<Extension>]
+    static member AddMiddleware(chatClient: ChatCompletionsChatClient, runMiddleware, ?streamingRunMiddleware) =
+        chatClient.AsBuilder().Use(runMiddleware, (streamingRunMiddleware |> Option.toObj)).Build()
+        |> fun chatClient -> new ChatCompletionsChatClient(chatClient)
 
 type Client =
     static member ForResponsesAPI(model, ?key, ?options) =
@@ -61,7 +52,7 @@ type Client =
                 OpenAI.OpenAIClientOptions(Endpoint = Uri(Environment.GetEnvironmentVariable "OPENAI_BASE_URL"))
             )
         OpenAI.OpenAIClient(key, options).GetResponsesClient(model).AsIChatClient()
-            |> fun chatClient -> new ChatCompletionsChatClient(chatClient) |> ChatCompletionsClient
+            |> fun chatClient -> new ResponsesChatClient(chatClient)
 
     static member ForChatCompletionsAPI(model, ?key, ?options) =
         let key =
@@ -73,5 +64,5 @@ type Client =
                 OpenAI.OpenAIClientOptions(Endpoint = Uri(Environment.GetEnvironmentVariable "OPENAI_BASE_URL"))
             )
         OpenAI.OpenAIClient(key, options).GetChatClient(model).AsIChatClient()
-            |> fun chatClient -> new ResponsesChatClient(chatClient) |> ResponsesClient
+            |> fun chatClient -> new ChatCompletionsChatClient(chatClient)
 
