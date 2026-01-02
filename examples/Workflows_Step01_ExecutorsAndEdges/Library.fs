5bc7aa117a0afe0b8f34cdabad03b54503a2eb12
Vladimir Shchur <odindafna2006@rambler.ru>
2026-01-01 16:24:38 -0800

    Workflows step01 baseline

---
+++ examples/Workflows_Step01_ExecutorsAndEdges/Library.fs
@@ -0,0 +1,40 @@
+﻿#nowarn "57"
+
+
+open System
+open System.Threading.Tasks
+open Microsoft.Agents.AI.Workflows
+open Shared
+
+module BaseLine =
+
+    type ReverseTextExecutor() =
+        inherit Executor<string, string>("ReverseTextExecutor")
+        override this.HandleAsync(message: string, context, cancellationToken) =
+            ValueTask.FromResult(String.Concat(message |> Seq.rev))
+
+    let run() =
+        let uppercaseFunc = Func<string, string>(fun (s: string) -> s.ToUpperInvariant())
+        let uppercaseExecutor = uppercaseFunc.BindAsExecutor("UppercaseExecutor")
+        let reverseExecutor = ReverseTextExecutor().BindExecutor()
+        let workflow =
+            WorkflowBuilder(uppercaseExecutor)
+                .AddEdge(uppercaseExecutor, reverseExecutor)
+                .WithOutputFrom(reverseExecutor)
+                .Build()
+        +task {
+            use! run = InProcessExecution.RunAsync(workflow, "Hello, World!")
+            for evt in run.NewEvents do
+                match evt with
+                | :? ExecutorCompletedEvent as executorComplete ->
+                    Console.WriteLine($"{executorComplete.ExecutorId}: {executorComplete.Data}")
+                | _ -> ()
+        }
+
+
+module Target =
+
+    let run() =
+        ()
+
+BaseLine.run()
---
+++ src/PipedAgents.MAF/Workflows.fs
@@ -0,0 +1,4 @@
+namespace PipedAgents.MAF
+
+
+
